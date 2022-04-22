6ab162fcf0280d15fb8a5c158db7d0c75e6bf95e
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2022-04-22 14:50:51 +0530

    add client folder for frontend

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+from locale import YESEXPR
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
 
-from .broadcast import ConnectionManager
+from broadcast import ConnectionManager
 
 app = FastAPI()
+app.mount('/client', StaticFiles(directory="client/dist"), name="static")
 
 manager = ConnectionManager()
 
+@app.get('/')
+async def index_page():
+    return FileResponse("client/dist/index.html")
+
 @app.websocket("/ws/{username}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
+async def websocket_endpoint(websocket: WebSocket, username: str):
     await manager.connect(websocket)
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.broadcast(f"Client #{username} says: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+        await manager.broadcast(f"Client #{username} left the chat")
